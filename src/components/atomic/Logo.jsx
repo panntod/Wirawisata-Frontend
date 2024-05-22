1cb944d07df1371eef7b3e4ac23af612f8a33f9c
panntod <pandhu.munjalindra@gmail.com>
2024-05-22 08:38:12 +0700

    feat: Create component logo

---
+++ src/components/atomic/Logo.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+import { H3 } from "../global/Text";
+
+const Logo = ({ color }) => (
+  <div className="flex items-center gap-2">
+    <img src="/logo.png" alt="logo pictures" className="w-12 h-8" />
+    <Link to="/" className={`border-l-2 border-${color}`}>
+      <H3 className={`ms-1 text-${color} text-lg font-bold`}>WiraWisata</H3>
+    </Link>
+  </div>
+);
+
+export default Logo;
