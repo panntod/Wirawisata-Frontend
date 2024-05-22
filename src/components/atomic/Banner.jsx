a537854ba972f61d23ae2d401939a5ad5dcd28f4
panntod <pandhu.munjalindra@gmail.com>
2024-05-23 06:49:10 +0700

    feat: Create banner component

---
+++ src/components/atomic/Banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => (
+  <section className="p-24 w-full">
+    <div className="rounded-2xl mx-auto h-[415px] w-[1240px] banner-gradient flex justify-center items-center">
+      <img src="/travel.png" alt="travel" className="w-[465px] h-[340px]" />
+      <span className="text-white text-4xl md:text-6xl font-bold">
+        Menjelajahi Keajaiban, Menciptakan Kenangan
+      </span>
+    </div>
+  </section>
+);
+
+export default Banner;
