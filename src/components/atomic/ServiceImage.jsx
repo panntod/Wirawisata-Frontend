2df1fdea5c60b3f279978eacaa599a7853660bd8
panntod <pandhu.munjalindra@gmail.com>
2024-05-22 08:01:13 +0700

    feat: Create component service image

---
+++ src/components/atomic/ServiceImage.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+import { H4 } from "../global/Text";
+
+const ServiceImage = (props) => (
+  <div>
+    <div className="bg-white rounded-full border-8 border-secondary">
+      <img
+        src={"/service" + props.img}
+        alt={props.title}
+        className="w-[130px] h-[130px] mx-auto"
+        width={120}
+        height={120}
+      />
+    </div>
+    <H4 className="text-white mt-4 text-center">{props.title}</H4>
+  </div>
+);
+
+export default ServiceImage;
