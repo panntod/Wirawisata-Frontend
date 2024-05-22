676c3a358496b8ef2ef4fc39c646e5713f7a5800
panntod <pandhu.munjalindra@gmail.com>
2024-05-22 08:01:49 +0700

    feat: Create landing page and service section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,80 @@
 import React from "react";
 
+import Banner from "@/components/atomic/Banner";
+import ServiceImage from "@/components/atomic/ServiceImage";
 import Navbar from "@/components/global/Navbar";
+import { H1 } from "@/components/global/Text";
 
 export default function Home() {
+  const firstContent = [
+    {
+      img: "/hotel.png",
+      title: "Hotel",
+    },
+    {
+      img: "/wisata.png",
+      title: "Wisata",
+    },
+    {
+      img: "/paket.png",
+      title: "Paket Wisata",
+    },
+  ];
+
+  const lastContent = [
+    {
+      img: "/kereta.png",
+      title: "Kereta",
+    },
+    {
+      img: "/bis.png",
+      title: "Bis",
+    },
+    {
+      img: "/kapal.png",
+      title: "Kapal",
+    },
+    {
+      img: "/pesawat.png",
+      title: "Pesawat",
+    },
+  ];
+
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <section className="h-screen flex flex-col md:flex-row gap-12 my-12">
+        <div className="w-1/2 p-12 flex flex-col items-center justify-center">
+          <h1 className="text-secondary text-4xl font-semibold sm:text-7xl mb-6">
+            Menggapai Destinasi Impian Bersama
+          </h1>
+
+          <img
+            src="/wirawisata.png"
+            alt="wirawisata"
+            className="w-[500px] h-[120px]"
+            width={600}
+            height={200}
+          />
+        </div>
+        <div className="justify-center items-center px-24 py-12 w-1/2">
+          <Banner />
+        </div>
+      </section>
+      <div className="h-screen relative custom-gradient flex flex-col justify-center items-center text-white">
+        <H1 className="mb-6">Our Service</H1>
+        <div className="flex gap-14 mb-6">
+          {firstContent.map(({ img, title }) => (
+            <ServiceImage key={`${title}-key-1`} title={title} img={img} />
+          ))}
+        </div>
+        <div className="flex gap-14">
+          {lastContent.map(({ img, title }) => (
+            <ServiceImage key={`${title}-key-2`} title={title} img={img} />
+          ))}
+        </div>
+        <div className="bg-custom" />
+      </div>
+    </>
   );
 }
