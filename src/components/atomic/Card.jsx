1b7faccf3388d231247a5cde28b7f68b677df00c
panntod <pandhu.munjalindra@gmail.com>
2024-05-23 09:44:48 +0700

    refactor: Change content into Children props

--- src/components/atomic/Card.jsx
+++ src/components/atomic/Card.jsx
@@ -1,33 +1,13 @@
-/* eslint-disable */
-
-import React from "react";
-
-import { H3, P } from "../global/Text";
 import { Link } from "react-router-dom";
 
-const Card = (props) => (
-  <>
-    <Link
-      to={props.link}
-      className="shadow-md bg-white w-72 rounded-3xl overflow-hidden"
-    >
-      <img
-        src={props.img}
-        alt={props.to}
-        className="w-full h-auto object-contain"
-      />
-      <div className="border-2 border-black rounded-b-3xl p-4">
-        <div className="mb-4">
-          <P className="text-xs">Berangkat Dari</P>
-          <H3>{props.from}</H3>
-        </div>
-        <div className="mb-4">
-          <P className="text-xs">Sampai</P>
-          <H3>{props.to}</H3>
-        </div>
-      </div>
-    </Link>
-  </>
+const Card = ({ link = "/", img, children }) => (
+  <Link
+    to={link}
+    className="shadow-md bg-white h-max w-72 rounded-3xl overflow-hidden"
+  >
+    <img src={img} alt="..." className="w-full h-auto object-contain" />
+    <div className="border-2 border-black rounded-b-3xl p-4">{children}</div>
+  </Link>
 );
 
 export default Card;
