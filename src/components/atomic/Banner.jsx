9351257eaa488d3451605aeee04d86ff9a2066ec
panntod <pandhu.munjalindra@gmail.com>
2024-05-23 10:10:35 +0700

    refactor: Make component banner responsive

--- src/components/atomic/Banner.jsx
+++ src/components/atomic/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => (
   <section className="p-24 w-full">
-    <div className="rounded-2xl mx-auto h-[415px] w-[1240px] banner-gradient flex justify-center items-center">
+    <div className="rounded-2xl mx-auto h-auto 2xl:h-[415px] w-auto 2xl:w-[1240px] banner-gradient flex flex-col xl:flex-row justify-center items-center p-12">
       <img src="/travel.png" alt="travel" className="w-[465px] h-[340px]" />
       <span className="text-white text-4xl md:text-6xl font-bold">
         Menjelajahi Keajaiban, Menciptakan Kenangan
